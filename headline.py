961f1edc3414e539e1a2207390177a228bf420d9
JatinRout <jatinrout@localhost.local>
2017-06-27 00:52:26 +0530

    css-style

--- headline.py
+++ headline.py
@@ -1,8 +1,8 @@
 import feedparser
 from flask import Flask
-from flask import render_template
+from flask import render_template,make_response
 from flask import request
-import json
+import json,datetime
 import urllib2
 import urllib
 
@@ -21,21 +21,34 @@ DEFAULTS = {'publication':'bbc',
             'currency_to':'USD'
 }
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
 @app.route("/")
 
 @app.route("/")
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    # get customised headlines, based on user input or default
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
-    # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+
+    # get customised weather based on user input or default
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
-    return render_template("home.html", articles=articles,weather=weather)
+
+    # get customised currency based on user input or default
+    # save cookies and return template
+    response = make_response(
+        render_template("home.html", articles=articles, weather=weather))
+    expires = datetime.datetime.now() + datetime.timedelta(days=2)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    return response
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
