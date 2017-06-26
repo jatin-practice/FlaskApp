bd55c939b1a63988ef30e0241a38ab74a05148c4
JatinRout <jatinrout@localhost.local>
2017-06-27 00:25:27 +0530

    Using GET

--- headline.py
+++ headline.py
@@ -2,23 +2,59 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib2
+import urllib
 
 app = Flask(__name__)
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=97fdf0129bbf8b17176abf092cada2fd"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=edcc367f8a5c4aabb995de942208b05d"
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'
+}
+
 @app.route("/")
-def get_news():
-        query = request.args.get("publication")
-        if not query or query.lower() not in RSS_FEEDS:
-                publication = "bbc"
-        else:
-                publication = query.lower()
-        feed = feedparser.parse(RSS_FEEDS[publication])
-        return render_template("home.html",articles=feed['entries'])
+
+@app.route("/")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles,weather=weather)
+
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS["publication"]
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
+def get_weather(query):
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description':parsed['weather'][0]['description'],'temperature':parsed['main']['temp'],'city':parsed['name']}
+    return weather
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
